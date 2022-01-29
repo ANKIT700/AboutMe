c2159476140f1c6636868d8839fa02deadd1dad2
Ankit-digimantra <ankit.parashar@digimantra.com>
2022-01-30 00:08:26 +0530

    "routing test"

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react"
 import logo from "./logo.svg"
 import "./App.css"
+import { Route,  BrowserRouter as Router, Routes } from "react-router-dom";
+import Navbar from "./components/header/Navbar";
 
-import RouteConfig from "./routes/RoutesConfig"
 
 
 
@@ -10,9 +11,37 @@ import RouteConfig from "./routes/RoutesConfig"
 class App extends Component {
   render() {
     return (
-     <RouteConfig/>
+      <>
+       <Router>
+       <Navbar />
+      <Routes>
+        <Route path='/' exact element={<Home/>} />
+        <Route path='/about' element={<Bus/>} />
+        <Route path='/events' element={<Cart/>} />
+        <Route path='/annual' element={<NotFound/>} />
+      </Routes>
+    </Router>
+      </>
     )
   }
 }
+
+function Home() {
+  return <h3>Home</h3>;
+}
+
+
+function Bus() {
+return <h3>Bus</h3>;
+}
+
+function Cart() {
+return <h3>Cart</h3>;
+}
+
+function NotFound() {
+  return <h3>NotFound</h3>;
+}
+
 
 export default App
